af110b9c10203ce7ea06c1beb0a91b9f18435d33
Christian Jairo Sarmiento <55115040+jairo8925@users.noreply.github.com>
2021-06-16 13:04:28 -0700

    added unique ID's for quiz and added answer field to Quiz

--- Web Quiz Engine/task/src/engine/Quiz.java
+++ Web Quiz Engine/task/src/engine/Quiz.java
@@ -1,8 +1,13 @@
 package engine;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Quiz {
+
+
+    @JsonProperty("id")
+    private int id;
 
     @JsonProperty("title")
     private String title;
@@ -13,12 +18,17 @@ public class Quiz {
     @JsonProperty("options")
     private String[] options;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private int answer;
+
     public Quiz() {}
 
-    public Quiz(String title, String text, String[] options) {
+    public Quiz(int id, String title, String text, String[] options, int answer) {
         this.title = title;
         this.text = text;
         this.options = options;
+        this.id = id;
+        this.answer = answer;
     }
 
     public String getText() {
@@ -33,6 +43,18 @@ public class Quiz {
         return options;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
     public void setText(String text) {
         this.text = text;
     }
@@ -43,5 +65,9 @@ public class Quiz {
 
     public void setOptions(String[] options) {
         this.options = options;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
--- Web Quiz Engine/task/src/engine/QuizController.java
+++ Web Quiz Engine/task/src/engine/QuizController.java
@@ -1,28 +1,66 @@
 package engine;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class QuizController {
 
-    private final Quiz quiz = new Quiz(
-            "The Java Logo",
-            "What is depicted on the Java logo?",
-            new String[]{"Robot", "Tea leaf", "Cup of coffee", "Bug"});
+    private final List<Quiz> quizzes = new ArrayList<>();
+    private int counter = 1;
 
-    @GetMapping("/api/quiz")
-    public Quiz getQuiz() {
-        return this.quiz;
+    @PostMapping("/api/quizzes")
+    public Quiz createQuiz(@RequestBody Quiz q) {
+        Quiz newQuiz = new Quiz(counter++, q.getTitle(), q.getText(), q.getOptions(), q.getAnswer());
+        quizzes.add(newQuiz);
+        return newQuiz;
     }
 
-    @PostMapping("/api/quiz")
-    public QuizResult answerQuiz(@RequestParam int answer) {
-        if (answer == 2) {
+    @GetMapping("/api/quizzes/{id}")
+    public Quiz getQuiz(@PathVariable int id) {
+        Quiz quiz = null;
+
+        for (Quiz q : quizzes) {
+            if (q.getId() == id) {
+                quiz = q;
+            }
+        }
+
+        if (quiz == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return quiz;
+    }
+
+    @GetMapping("/api/quizzes")
+    public List<?> getAllQuizzes() {
+        if (quizzes.isEmpty()) {
+            return List.of();
+        }
+        return quizzes;
+    }
+
+    @PostMapping(value = "/api/quizzes/{id}/solve", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public QuizResult answerQuiz(@PathVariable int id, @RequestParam int answer) {
+        Quiz quiz = null;
+
+        for (Quiz q : quizzes) {
+            if (q.getId() == id) {
+                quiz = q;
+            }
+        }
+
+        if (quiz == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        if (answer == quiz.getAnswer()) {
             return new QuizResult(true, "Congratulations, you're right!");
         } else {
             return new QuizResult(false, "Wrong answer! Please, try again.");
